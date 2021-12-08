c2cbefcf17f80c38ed8db6c23ba703942d372b14
vasyl_artus <basa94389@gmail.com>
2021-12-08 12:22:36 +0200

    link repair 3

--- src/routes/Menu/Menu.jsx
+++ src/routes/Menu/Menu.jsx
@@ -7,7 +7,7 @@ export default function Menu() {
     <nav className="menu">
     <Link to="/21-homework/" className="link-nav">Home</Link>
     <Link to="/21-homework/contacts" className="link-nav">Contacts</Link>
-    <Link to="/posts" className="link-nav">Posts</Link>
+    <Link to="/21-homework/posts" className="link-nav">Posts</Link>
     <Link to="/21-homework/photos" className="link-nav">Photos</Link>
 </nav>
   );
